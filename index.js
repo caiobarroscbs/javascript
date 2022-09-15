c1e147214ca6c9cc728ab6d6c5f8f3033026146e
caiobarroscbs <caiobarros.contato@gmail.com>
2022-09-15 18:11:30 -0300

    Meu primeiro repositorio

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * Tipos primitivos 
+ * variaveis no Javascript
+ * Operadores matemáticos do javascript
+ *  
+ */
+// serve para escrever mensagens no console
+// string - textos
+// number - números 
+// boolean - verdadeiro/falso
+
+console.log('Hello world')
+console.log(5)
+console.log(43.76)
+console.log(true)
+console.log(false)
+
+//variáveis
+let mensagem = 'soulcode BC 25 JFS'
+
+console.log(mensagem)
