09273bf48136e8d9ed991ad1b4852d4dce5c879c
caiobarroscbs <caiobarros.contato@gmail.com>
2022-09-16 17:11:32 -0300

    operadores de incremento e decremento

--- operadores_de_incremento.js
+++ operadores_de_incremento.js
@@ -4,6 +4,7 @@ const nomeUsuario = prompt('Olá, usuário, qual o seu nome?') // mostra uma men
 let num = Number (prompt('Olá, ' + nomeUsuario + '. Digite algum número, por favor'))
 console.log(typeof num) // typeof retorna o tipo do valor que a variavel armazena
 
+// operadores de incremento
 const sucessor = num +1
 const antecessor = num -1
 
