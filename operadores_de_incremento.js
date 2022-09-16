ff9330d5451b2a000940a73449a257ef0a18d272
caiobarroscbs <caiobarros.contato@gmail.com>
2022-09-16 18:08:17 -0300

    operadores e decremento

--- operadores_de_incremento.js
+++ operadores_de_incremento.js
@@ -1,12 +1,18 @@
-alert (' Código Funcionando!') // mostrar uma mensagem para o usuario
-const nomeUsuario = prompt('Olá, usuário, qual o seu nome?') // mostra uma mensagem e recebe um valor+
+alert('Código Funcionando!') // mostrar uma mensagem para o usuário
+const nomeUsuario = prompt('Olá, usuário, qual o seu nome?') // mostra uma mensagem e recebe um valor
 
-let num = Number (prompt('Olá, ' + nomeUsuario + '. Digite algum número, por favor'))
-console.log(typeof num) // typeof retorna o tipo do valor que a variavel armazena
+let num = Number(prompt('Olá, ' + nomeUsuario + '. Digite algum número, por favor')) // 5
+//console.log(typeof num) // typeof retorna o tipo do valor que a variável armazena
 
 // operadores de incremento
-const sucessor = num +1
-const antecessor = num -1
+//const sucessor = num++ // = num + 1 
 
-alert('O sucessor de ' + num + ' é ' + sucessor)
-alert('O sucessor de ' + num + ' é ' + antecessor)
+// alert('O sucessor de ' + num + ' é ' + (num + 1))
+console.log(num++)// 5 // operador de pós-incremento
+console.log(num) // 6
+console.log(++num) // 7 // operador de pré-incremento
+
+// operadores de decremento
+console.log(num--) // 7 // operador de pós-decremento
+console.log(num) // 6
+console.log(--num) // 5 // operador de pré-decremento
